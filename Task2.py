b9c3babc61dbc2e3a15ae7c77ec5b76455d5fb0c
Mateusser <mateusser@gmail.com>
2014-10-14 22:00:30 -0700

    added images

--- Task2.py
+++ Task2.py
@@ -1,5 +1,8 @@
 from core_crypto import *
+from conversions import *
 import base64
+
+#####################    TASK A    ####################
 
 key = "CALIFORNIA LOVE!"
 
@@ -7,11 +10,29 @@ fp = open("Lab2.TaskII.A.txt", "r")
 cipherb64 = fp.read()
 fp.close()
 
-cipher = base64.b64decode(cipherb64)
+cipher = b64_to_ascii(cipherb64)
 
-paddedPlain = ecb_decrypt(cipher, key)
+plain = ecb_decrypt(cipher, key)
 
-plain = pkcs7_strip(paddedPlain, AES.block_size)
+#print plain
 
-print paddedPlain
-	
+#####################    TASK B    ####################
+
+fp = open("Lab2.TaskII.B.txt", "r")
+fileContaint = fp.read()
+fp.close()
+
+ciphersHEX = fileContaint.split()
+
+ciphers = [0] * len(ciphersHEX)
+
+for i in range(len(ciphersHEX)):
+	ciphers[i] = hex_to_ascii(ciphersHEX[i])
+
+	fp = open("./images/image"+str(i)+".bmp", "w")
+	fp.write(ciphers[i])
+	fp.close()
+
+#plain = ecb_decrypt(cipher, key)
+
+#print plain
