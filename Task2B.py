23a8872cd82e482a27e41ab7b527d8156b61e32d
Matheus Faria <matheus.sousa.faria@gmail.com>
2014-10-23 19:19:01 -0700

    task 2B - working for lsb(26)

--- Task2B.py
+++ Task2B.py
@@ -1,19 +1,22 @@
-import hashlib, sys
 from conversions import *
 from core_crypto import *
 
-hashDict = {}  
+hashDict = {}
 total = 10000000
 
 for i in xrange(1,total):
-    hashd = hashlib.sha1(str(i)).hexdigest()
-    relev = hashd[(len(hashd)-7):]		# 7 bytes, 56 bits
-    mask  = "03ffffffffffff"			# mask to ignore 6 most significant bits
-    relev = xor_strings(relev, hex_to_ascii(mask))
+    hashd = sha1_int(str(i))
+
+    relev = least_50bits = bin(hashd)[-26:]
+
+    #relev = hashd[(len(hashd)-7):]      # 7 bytes, 56 bits
+    #mask  = "03ffffffffffff"            # mask to ignore 6 most significant bits
+    #relev = xor_strings(relev, hex_to_ascii(mask))
+
     if relev in hashDict:
         print " *** Collision found ", hashDict[relev], " and ", i
         break
     hashDict[relev] = i
 
-print hashDict[relev], " = "+hashlib.sha1(str(hashDict[relev])).hexdigest()
-print i, " = "+hashlib.sha1(str(i)).hexdigest()
+print hashDict[relev], " = "+sha1_hex(str(hashDict[relev]))
+print i, " = "+sha1_hex(str(i))
