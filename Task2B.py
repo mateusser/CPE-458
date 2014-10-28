bb82a01eef8d0a089ef0cbdabfd1ae795df7e268
Mateusser <mateusser@gmail.com>
2014-10-28 16:22:51 -0700

    added coliding bits

--- Task2B.py
+++ Task2B.py
@@ -16,3 +16,8 @@ for i in xrange(1, total):
 
 print hashDict[relev], " = "+sha1_hex(str(hashDict[relev]))
 print i, " = "+sha1_hex(str(i))
+
+# *** Collision found  23612730  and  72349653
+#23612730  = 48cf792af65204f61439c92df40eb77a7129a491
+#72349653  = 58c955b55efe3cfe3a1b0c7b2a5eb77a7129a491
+#										eb77a7129a491 = 52 bits
