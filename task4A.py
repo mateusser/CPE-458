a5b63b97d2659895fbeb381ff062c3f3adf83807
Matheus Faria <matheus.sousa.faria@gmail.com>
2014-10-29 10:45:57 -0700

    Task 4A - Solution that takes too long

--- task4A.py
+++ task4A.py
@@ -32,59 +32,27 @@ def sendMessageAndMac(q, mac):
 
 message = 'Matheus'
 best_mac = guessed_mac = 'a'*40
+
 max_time = 0
 get_out_now = False
+
 for i in range(0, 40, 2):
     print '\n\n'
     print '='*80
     print i
-    prev_time = max_time
-    max_time = 0
+
+    prev_time = sendMessageAndMac(message, best_mac)
     guessed_mac = best_mac
     total_time = 0
+    max_time = 0
 
-    above_average = []
-    for j in range(2**8):
-        hex_number = '%02x' % (j)
-        guessed_mac = guessed_mac[:i] + hex_number + guessed_mac[i + 2:]
-        time_mesure = sendMessageAndMac(message, guessed_mac)
-
-        if time_mesure > prev_time:
-            total_time += time_mesure
-            above_average.append(hex_number)
-
-        if time_mesure == -1:
-            get_out_now = True
-            break
-
-        if time_mesure > max_time:
-            max_time = time_mesure
-            best_mac = guessed_mac
-
-    if get_out_now:
-        break
-
-    while len(above_average) > 1:
-        print
-        print
-        print "Total of elementes above average ", len(above_average)
-        print "Mean:", total_time/len(above_average)
-        print
-
-        hex_numbers = above_average
-        mean = total_time/len(above_average)
-
-        above_average = []
-        total_time = 0
+    hex_dict = {}
+    for k in range(30):
         max_time = 0
-        for hexn in hex_numbers:
-            guessed_mac = guessed_mac[:i] + hexn + guessed_mac[i + 2:]
+        for j in range(2**8):
+            hex_number = '%02x' % (j)
+            guessed_mac = guessed_mac[:i] + hex_number + guessed_mac[i + 2:]
             time_mesure = sendMessageAndMac(message, guessed_mac)
-
-            if time_mesure > mean:
-                above_average.append(hexn)
-                total_time += time_mesure
-                print hexn, time_mesure
 
             if time_mesure == -1:
                 get_out_now = True
@@ -93,9 +61,27 @@ for i in range(0, 40, 2):
             if time_mesure > max_time:
                 max_time = time_mesure
                 best_mac = guessed_mac
+                choosen_hex = hex_number
+
+        if choosen_hex in hex_dict:
+            hex_dict[choosen_hex] += 1
+        else:
+            hex_dict[choosen_hex] = 1
+
+        print best_mac, max_time
 
         if get_out_now:
             break
+
+    print
+    print hex_dict
+    print
+    maxn = 0
+    for hex_number in hex_dict:
+        if hex_dict[hex_number] > maxn:
+            choosen_hex = hex_number
+            maxn = hex_dict[hex_number]
+    best_mac = guessed_mac[:i] + choosen_hex + guessed_mac[i + 2:]
 
     print '\n\nFinal:'
     print max_time
